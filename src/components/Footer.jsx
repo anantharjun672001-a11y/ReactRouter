d65ae83e36029276588c9ea0470525814a2bc749
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-12-26 10:03:47 +0530

    footer completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-
+            <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-pink-200 border-b-pink-100 border-default shadow-sm md:flex md:items-center md:justify-between md:p-6">
+                <span className="text-sm text-body text-purple-600 sm:text-center">© 2023 Shopsy ! . All Rights Reserved.
+                </span>
+                
+            </footer>
 
         </div>
     );
